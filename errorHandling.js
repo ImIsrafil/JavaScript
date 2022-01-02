d2c08bb9125b2d6954cf18008ef395e1c44542f9
ImIsrafil <im.israfil.fi@gmail.com>
2022-01-02 12:55:27 +0600

    I need two more days to finish my goal inshaallah

---
+++ errorHandling.js
@@ -0,0 +1,15 @@
+
+try {
+    // code test
+
+    console.log("Hello World");
+    console.log(x);
+    console.log("Hello again");
+
+} catch (err) {
+    console.log("Error Occurred");
+    console.log(err.name);
+    console.log(err.message); // error handle
+} finally {
+    console.log("This is Israfil");
+}
